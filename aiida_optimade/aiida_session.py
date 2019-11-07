c77c4fcf7c060dc28fcaeeeab8e73cc6ae060243
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-11-07 18:50:45 +0100

    Created custom backend and QB for SessionLocal

--- aiida_optimade/aiida_session.py
+++ aiida_optimade/aiida_session.py
@@ -1,13 +1,55 @@
+import json
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from aiida.orm.implementation.django.querybuilder import DjangoQueryBuilder
+from aiida.orm.implementation.django.backend import DjangoBackend
+
+from aiida_optimade.main import profile
+
+
+separator = ":" if profile.database_port else ""
+engine_url = "postgresql://{user}:{password}@{hostname}{separator}{port}/{name}".format(
+    separator=separator,
+    user=profile.database_username,
+    password=profile.database_password,
+    hostname=profile.database_hostname,
+    port=profile.database_port,
+    name=profile.database_name,
+)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    engine_url,
+    json_serializer=json.dumps,
+    json_deserializer=json.loads,
+    encoding="utf-8",
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+class OptimadeDjangoQueryBuilder(DjangoQueryBuilder):
+    """New DjangoQueryBuilder"""
+
+    def __init__(self, backend, session):
+        DjangoQueryBuilder.__init__(self, backend)
+        self.__optimade_session = session
+
+    def get_session(self):
+        return self.__optimade_session
+
+
+class OptimadeDjangoBackend(DjangoBackend):
+    """New DjangoBackend"""
+
+    def __init__(self):
+        super().__init__()
+        self.__optimade_session = SessionLocal()
+
+    def query(self):
+        """Special OPTiMaDe query()"""
+        return OptimadeDjangoQueryBuilder(self, self.__optimade_session)
+
+    def close(self):
+        self.__optimade_session.close()
