a11e90f46f3a8088c3ce89a3504e3ffc80541843
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-11-12 19:53:59 +0100

    Update config

--- aiida_optimade/config.py
+++ aiida_optimade/config.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict, Set
 from pathlib import Path
 
 from optimade.server.config import Config
@@ -13,12 +14,14 @@ class ServerConfig(Config):
 
     page_limit = 500
     db_page_limit = 1000
-    index_base_url = None
-    provider = "_aiida_"
-    provider_name = "AiiDA"
-    provider_description = "AiiDA: Automated Interactive Infrastructure and Database for Computational Science (http://www.aiida.net)"
-    provider_homepage = "http://www.aiida.net"
-    provider_fields = set()
+    provider = {
+        "prefix": "_aiida_",
+        "name": "AiiDA",
+        "description": "AiiDA: Automated Interactive Infrastructure and Database for Computational Science (http://www.aiida.net)",
+        "homepage": "http://www.aiida.net",
+        "index_base_url": None,
+    }
+    provider_fields: Dict[str, Set] = {}
 
     def __init__(self, ftype: str = None, filename: str = None):
         self.FILENAME = self.FILENAME if filename is None else filename
@@ -36,12 +39,7 @@ class ServerConfig(Config):
         self.db_page_limit = int(config.get("db_page_limit", self.db_page_limit))
         self.index_base_url = config.get("index_base_url", self.index_base_url)
         self.provider = config.get("provider", self.provider)
-        self.provider_name = config.get("provider_name", self.provider_name)
-        self.provider_description = config.get(
-            "provider_description", self.provider_description
-        )
-        self.provider_homepage = config.get("provider_homepage", self.provider_homepage)
-        self.provider_fields = set(config.get("provider_fields", self.provider_fields))
+        self.provider_fields = config.get("provider_fields", self.provider_fields)
 
 
 CONFIG = ServerConfig()
