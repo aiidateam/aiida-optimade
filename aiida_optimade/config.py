7543c24133edee9228aa96739d43f374fbc61921
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2019-12-12 19:54:12 +0100

    Use server.cfg as implemented in tools (#14)
    Use server.cfg as implemented in tools.
    Update codecov.

--- aiida_optimade/config.py
+++ aiida_optimade/config.py
@@ -1,46 +1,55 @@
 import json
-from typing import Dict, Set
+from typing import Any
 from pathlib import Path
 
-from optimade.server.config import Config
+from optimade.server.config import Config, NoFallback
 
 
 class ServerConfig(Config):
     """Load config file"""
 
-    ftype = "json"
+    @staticmethod
+    def _DEFAULTS(field: str) -> Any:
+        res = {
+            "version": "v1.0.0",
+            "page_limit": 100,
+            "db_page_limit": 500,
+            "provider": {
+                "prefix": "_aiida_",
+                "name": "AiiDA",
+                "description": "AiiDA: Automated Interactive Infrastructure and Database for Computational Science (http://www.aiida.net)",
+                "homepage": "http://www.aiida.net",
+                "index_base_url": None,
+            },
+            "provider_fields": {},
+        }
+        if field not in res:
+            raise NoFallback(f"No fallback value found for '{field}'")
+        return res[field]
 
-    FILENAME = "config"
-
-    version = "v1.0.0"
-    page_limit = 500
-    db_page_limit = 1000
-    provider = {
-        "prefix": "_aiida_",
-        "name": "AiiDA",
-        "description": "AiiDA: Automated Interactive Infrastructure and Database for Computational Science (http://www.aiida.net)",
-        "homepage": "http://www.aiida.net",
-        "index_base_url": None,
-    }
-    provider_fields: Dict[str, Set] = {}
-
-    def __init__(self, ftype: str = None, filename: str = None):
-        self.FILENAME = self.FILENAME if filename is None else filename
-        super().__init__(ftype)
+    def __init__(self, server_cfg: Path = None):
+        server = (
+            Path(__file__).resolve().parent.parent.joinpath("server.cfg")
+            if server_cfg is None
+            else server_cfg
+        )
+        super().__init__(server)
 
     def load_from_json(self):
         """Load parameters from FILENAME.json"""
 
-        with open(
-            Path(__file__).resolve().parent.joinpath(self.FILENAME + ".json")
-        ) as config_file:
+        with open(self._path) as config_file:
             config = json.load(config_file)
 
-        self.version = config.get("version", self.version)
-        self.page_limit = int(config.get("page_limit", self.page_limit))
-        self.db_page_limit = int(config.get("db_page_limit", self.db_page_limit))
-        self.provider = config.get("provider", self.provider)
-        self.provider_fields = config.get("provider_fields", self.provider_fields)
+        self.version = config.get("version", self._DEFAULTS("version"))
+        self.page_limit = int(config.get("page_limit", self._DEFAULTS("page_limit")))
+        self.db_page_limit = int(
+            config.get("db_page_limit", self._DEFAULTS("db_page_limit"))
+        )
+        self.provider = config.get("provider", self._DEFAULTS("provider"))
+        self.provider_fields = config.get(
+            "provider_fields", self._DEFAULTS("provider_fields")
+        )
 
 
 CONFIG = ServerConfig()
