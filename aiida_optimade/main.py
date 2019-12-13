c3935be7c3f5a14a6294890e541ee4f97f0fcd7a
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-12-13 11:01:10 +0100

    Use new SQLABackend methods for sessions

--- aiida_optimade/main.py
+++ aiida_optimade/main.py
@@ -38,30 +38,31 @@ async def backend_middleware(request: Request, call_next):
     """Use custom AiiDA backend for all requests"""
     response = None
     try:
-        if PROFILE.database_backend == "django":
-            from aiida_optimade.aiida_session import (
-                OptimadeDjangoBackend as OptimadeBackend,
-            )
+        # if PROFILE.database_backend == "django":
+        #     from aiida_optimade.aiida_session import (
+        #         OptimadeDjangoBackend as OptimadeBackend,
+        #     )
 
-            from warnings import warn
+        #     from warnings import warn
 
-            warn(
-                "The django backend does not support the special 1 AiiDA DB session per 1 HTTP request implemented in this package!"
-            )
+        #     warn(
+        #         "The django backend does not support the special 1 AiiDA DB session per 1 HTTP request implemented in this package!"
+        #     )
 
-        elif PROFILE.database_backend == "sqlalchemy":
-            from aiida_optimade.aiida_session import (
-                OptimadeSqlaBackend as OptimadeBackend,
-            )
-        else:
-            raise AiidaError(
-                f'Unknown AiiDA backend "{PROFILE.database_backend}" for profile {PROFILE}'
-            )
+        # elif PROFILE.database_backend == "sqlalchemy":
+        #     from aiida_optimade.aiida_session import (
+        #         OptimadeSqlaBackend as OptimadeBackend,
+        #     )
+        # else:
+        #     raise AiidaError(
+        #         f'Unknown AiiDA backend "{PROFILE.database_backend}" for profile {PROFILE}'
+        #     )
+        from aiida.manage.manager import get_manager
 
-        request.state.backend = OptimadeBackend()
+        request.state.backend = get_manager().get_backend()
         response = await call_next(request)
     finally:
-        request.state.backend.close()
+        request.state.backend.close_session()
 
     if response:
         return response
