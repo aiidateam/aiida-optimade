00015a6f78c1c3eef216c6472d4dbf1ddf03afe3
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-01-30 13:48:48 +0100

    Use update_file utility function in tasks

--- tasks.py
+++ tasks.py
@@ -1,8 +1,19 @@
 import re
+from typing import Tuple
 
 from invoke import task
 
 from aiida_optimade import __version__
+
+
+def update_file(filename: str, sub_line: Tuple[str, str], strip: str = None):
+    """Utility function for tasks to read, update, and write files"""
+    with open(filename, "r") as handle:
+        lines = [re.sub(sub_line[0], sub_line[1], l.rstrip(strip)) for l in handle]
+
+    with open(filename, "w") as handle:
+        handle.write("\n".join(lines))
+        handle.write("\n")
 
 
 @task
@@ -12,42 +23,21 @@ def setver(_, patch=False, new_ver=""):
     if (not patch and not new_ver) or (patch and new_ver):
         raise Exception(
             "Either use --patch or specify e.g. "
-            "--new-ver='Major.Minor.Patch(a|b|rc)?[0-9]+'"
+            '--new-ver="Major.Minor.Patch(a|b|rc)?[0-9]+"'
         )
     if patch:
         ver = [int(x) for x in __version__.split(".")]
         ver[2] += 1
         new_ver = ".".join(map(str, ver))
-    with open("aiida_optimade/__init__.py", "r") as handle:
-        lines = [
-            re.sub("__version__ = .+", '__version__ = "{}"'.format(new_ver), l.rstrip())
-            for l in handle
-        ]
-    with open("aiida_optimade/__init__.py", "w") as handle:
-        handle.write("\n".join(lines))
-        handle.write("\n")
 
-    with open("setup.json", "r") as handle:
-        lines = [
-            re.sub(
-                '"version": ([^,]+),', '"version": "{}",'.format(new_ver), l.rstrip()
-            )
-            for l in handle
-        ]
-    with open("setup.json", "w") as handle:
-        handle.write("\n".join(lines))
-        handle.write("\n")
-
-    with open("aiida_optimade/config.json", "r") as handle:
-        lines = [
-            re.sub(
-                '"version": ([^,]+),', '"version": "{}",'.format(new_ver), l.rstrip()
-            )
-            for l in handle
-        ]
-    with open("aiida_optimade/config.json", "w") as handle:
-        handle.write("\n".join(lines))
-        handle.write("\n")
+    update_file(
+        "aiida_optimade/__init__.py", ("__version__ = .+", f'__version__ = "{new_ver}"')
+    )
+    update_file("setup.json", ('"version": ([^,]+),', f'"version": "{new_ver}",'))
+    update_file(
+        "aiida_optimade/config.json",
+        ('"version": ([^,]+),', f'"version": "{new_ver}",'),
+    )
 
     print("Bumped version to {}".format(new_ver))
 
@@ -59,54 +49,24 @@ def optimade_req(_, ver=""):
     if not ver:
         raise Exception("Please specify --ver='Major.Minor.Patch'")
 
-    with open("setup.py", "r") as handle:
-        lines = [
-            re.sub("optimade~=([^,]+)", f'optimade~={ver}"', l.rstrip()) for l in handle
-        ]
-    with open("setup.py", "w") as handle:
-        handle.write("\n".join(lines))
-        handle.write("\n")
-
-    with open("README.md", "r") as handle:
-        lines = [
-            re.sub(
-                "https://raw.githubusercontent.com/Materials-Consortia/"
-                "optimade-python-tools/v([^,]+)/.ci/",
-                "https://raw.githubusercontent.com/Materials-Consortia/"
-                f"optimade-python-tools/v{ver}/.ci/",
-                l.rstrip("\n"),
-            )
-            for l in handle
-        ]
-    with open("README.md", "w") as handle:
-        handle.write("\n".join(lines))
-        handle.write("\n")
-
-    with open("Dockerfile", "r") as handle:
-        lines = [
-            re.sub(
-                "OPTIMADE_TOOLS_VERSION=.*", f"OPTIMADE_TOOLS_VERSION={ver}", l.rstrip()
-            )
-            for l in handle
-        ]
-
-    with open("Dockerfile", "w") as handle:
-        handle.write("\n".join(lines))
-        handle.write("\n")
-
+    update_file("setup.py", (r"optimade\[mongo\]~=([^,]+)", f'optimade[mongo]~={ver}"'))
+    update_file(
+        "README.md",
+        (
+            "https://raw.githubusercontent.com/Materials-Consortia/"
+            "optimade-python-tools/v([^,]+)/.ci/",
+            "https://raw.githubusercontent.com/Materials-Consortia/"
+            f"optimade-python-tools/v{ver}/.ci/",
+        ),
+        strip="\n",
+    )
+    update_file(
+        "Dockerfile", ("OPTIMADE_TOOLS_VERSION=.*", f"OPTIMADE_TOOLS_VERSION={ver}")
+    )
     for file_format in ("j2", "yml"):
-        with open(f"profiles/docker-compose.{file_format}", "r") as handle:
-            lines = [
-                re.sub(
-                    "OPTIMADE_TOOLS_VERSION: .*",
-                    f"OPTIMADE_TOOLS_VERSION: {ver}",
-                    l.rstrip(),
-                )
-                for l in handle
-            ]
-
-        with open(f"profiles/docker-compose.{file_format}", "w") as handle:
-            handle.write("\n".join(lines))
-            handle.write("\n")
+        update_file(
+            f"profiles/docker-compose.{file_format}",
+            ("OPTIMADE_TOOLS_VERSION: .*", f"OPTIMADE_TOOLS_VERSION: {ver}"),
+        )
 
     print("Bumped OPTiMaDe Python Tools version requirement to {}".format(ver))
