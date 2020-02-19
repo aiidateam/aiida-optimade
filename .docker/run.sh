6dd56ea8a016b32d0420158b59c7f0f36c04d2b6
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-02-19 11:51:04 +0100

    Revert to docker.host.internal

--- .docker/run.sh
+++ .docker/run.sh
@@ -3,9 +3,9 @@ set -ex
 mkdir -p $AIIDA_PATH/.aiida
 cp -n /profiles/$AIIDA_PROFILE.json $AIIDA_PATH/.aiida/config.json
 
-# make host.docker.internal available
+# make docker.host.internal available
 # see https://github.com/docker/for-linux/issues/264#issuecomment-387525409
 # ltalirz: Only works for Mac, not Linux
-# echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\thost.docker.internal" | tee -a /etc/hosts > /dev/null
+# echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\tdocker.host.internal" | tee -a /etc/hosts > /dev/null
 
 uvicorn aiida_optimade.main:APP --host 0.0.0.0 --port 80
