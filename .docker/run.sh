3ac44987bcd902b510fbde586a79ef44bda2caf6
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-05-26 11:09:22 +0200

    Add logging level to Dockerfile

--- .docker/run.sh
+++ .docker/run.sh
@@ -22,4 +22,10 @@ fi
 aiida-optimade --profile ${AIIDA_PROFILE} init ${FORCE_INIT} ${USE_MONGO} ${load_mongo_file}
 
 # Run (uvicorn) server
-aiida-optimade --profile ${AIIDA_PROFILE} run --host 0.0.0.0 --port 80 --log-level info
+if [ -n "${OPTIMADE_LOGGING_LEVEL}" ]; then
+    LOG_LEVEL="${OPTIMADE_LOGGING_LEVEL}"
+else
+    LOG_LEVEL=info
+fi
+
+aiida-optimade --profile ${AIIDA_PROFILE} run --host 0.0.0.0 --port 80 --log-level ${LOG_LEVEL}
