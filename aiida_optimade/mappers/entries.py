10d5027be760f89b60bef1d774de2ac265b3c4b8
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-07-18 02:31:00 +0200

    Move general map_back method to parent cls

--- aiida_optimade/mappers/entries.py
+++ aiida_optimade/mappers/entries.py
@@ -20,7 +20,9 @@ class ResourceMapper(OptimadeResourceMapper):
 
     @classmethod
     def all_aliases(cls) -> Tuple[Tuple[str, str]]:
-        """Get all aliases as a tuple"""
+        """Get all aliases as a tuple
+        Also add `PROJECT_PREFIX` fields to the tuple
+        """
         res = super(ResourceMapper, cls).all_aliases()
         return res + tuple(
             (field, f"{cls.PROJECT_PREFIX}{field}")
@@ -38,6 +40,36 @@ class ResourceMapper(OptimadeResourceMapper):
         :return: A resource object in OPTIMADE format
         :rtype: dict
         """
+        mapping = ((real, alias) for alias, real in cls.all_aliases())
+
+        new_object_attributes = {}
+        new_object = {}
+
+        for real, alias in mapping:
+            if (
+                real in entity_properties
+                and entity_properties[real] is not None
+                and alias not in cls.TOP_LEVEL_NON_ATTRIBUTES_FIELDS
+            ):
+                new_object_attributes[alias] = entity_properties[real]
+
+        # We always need "id"
+        if "id" not in entity_properties:
+            raise KeyError(
+                f'"id" should be present in entity_properties: {entity_properties}'
+            )
+
+        for field in cls.TOP_LEVEL_NON_ATTRIBUTES_FIELDS:
+            value = entity_properties.get(field, None)
+            if value is not None:
+                new_object[field] = value
+
+        new_object["attributes"] = cls.build_attributes(
+            new_object_attributes, new_object["id"]
+        )
+        new_object["type"] = cls.ENDPOINT
+
+        return new_object
 
     @classmethod
     def build_attributes(cls, retrieved_attributes: dict, entry_pk: int) -> dict:
