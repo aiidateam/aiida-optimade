460bb60551b14b6ab92ef7d111f324d0f64849c8
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-02-17 22:16:55 +0100

    Add MongoDB configurations for Materials Cloud
    Make sure CI always runs on this branch.

--- .github/mongo/load_data.py
+++ .github/mongo/load_data.py
@@ -6,7 +6,7 @@ import bson.json_util
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017")
-collection = client["aiida_optimade"]["structures"]
+collection = client["test_django"]["structures"]
 
 with open(Path(__file__).parent.joinpath("test_structures_mongo.json")) as handle:
     data = bson.json_util.loads(handle.read())
